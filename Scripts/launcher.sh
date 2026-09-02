b67ebc4ee7e777568bcd1989f024e06ab22e80be
Manofox-br <agelo7439@gmail.com>
2026-09-01 21:29:18 -0300

    Criar launcher.sh

---
+++ Scripts/launcher.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Navegador
+firefox &
+
+# Editor de texto
+xed &
+
+# Terminal
+gnome-terminal &
+
+# Gerenciador de arquivos
+nemo &
+
+# Player de música
+rhythmbox &
+
+# Player de vídeo
+vlc &
+
+# Suite Office
+libreoffice &
+
+# Calculadora
+gnome-calculator &
+
+# Visualizador de imagens
+pix &
+
+# Editor de imagens
+gimp &
+
+# Configurações do sistema
+cinnamon-settings &
+
+# Monitor de sistema
+gnome-system-monitor &
+
+# Gerenciador de pacotes
+synaptic &
+
+# Gerenciador de atualizações
+mintupdate &
+
+# Gerenciador de drivers
+mintdrivers &
+
+# Gerenciador de software
+mintinstall &
+
+# Ferramenta de backup
+timeshift &
+
+# Ferramenta de discos
+gnome-disks &
